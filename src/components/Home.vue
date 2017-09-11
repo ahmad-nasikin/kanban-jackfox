b82ee2a874b4fd41de0cb87fc46814ca9d716f86
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-09-11 17:50:00 +0700

    mecah detail jadi componen, baru di task Log

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,52 +1,9 @@
 <template lang="html">
   <div class="container">
-    <div class="row-master">
-      <div class="row1">
-        <div class="container-offset-10">
-          <!-- <button type="button" name="button" class="btn-warning glyphicon glyphicon-pencil"> Add Task</button> -->
-              <!-- Modal content-->
-          <div>
-              <!-- Trigger the modal with a button -->
-              <button id="create" type="button" class="btn btn-danger glyphicon glyphicon-pencil" data-toggle="modal" data-target="#myModal"> Add Task</button>
-              <!-- Modal -->
-              <div id="myModal" class="modal fade" role="dialog">
-                <div class="modal-dialog">
-                  <!-- Modal content-->
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <button type="button" class="close" data-dismiss="modal">&times;</button>
-                      <h4 class="modal-title">New Task</h4>
-                    </div>
-                    <div class="modal-body">
-                      <div class="form-group">
-                        <label class="control-label" for="focusedInput">Input Title</label>
-                        <input class="form-control" id="focusedInput" type="text" placeholder="Input Title" v-model="title">
-                      </div>
-                      <div class="form-group">
-                        <label for="textArea" class="control-label">Description</label>
-                        <div>
-                          <textarea class="form-control" id="textArea" v-model="description"></textarea>
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <label class="control-label" for="focusedInput">Point</label>
-                        <input class="form-control" id="focusedInput" type="text" v-model="point">
-                      </div>
-                      <div class="form-group">
-                        <label class="control-label" for="focusedInput">Assign To</label>
-                        <input class="form-control" id="focusedInput" type="text" placeholder="Assign To" v-model="assign_to">
-                      </div>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-danger" data-dismiss="modal"> Cancel</button>
-                      <button type="button" class="btn btn-success" data-dismiss="modal" @click="addTask"> Save</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-        </div>
-      </div><br>
+    <div class="row master">
+      <div class="row-1">
+          <modal></modal><br>
+      </div>
       <div class="row-2">
         <div class="col-md-3">
           <log></log>
@@ -57,11 +14,11 @@
         <div class="col-md-3">
           <doing></doing>
       </div>
-        </div>
         <div class="col-md-3">
           <done></done>
         </div>
       </div>
+    </div>
     </div>
   </div>
 </template>
@@ -71,35 +28,14 @@ import log from '@/components/Log'
 import todo from '@/components/Todo'
 import doing from '@/components/Doing'
 import done from '@/components/Done'
+import modal from '@/components/Modal'
 export default {
   components: {
     log,
     todo,
     doing,
-    done
-  },
-  data () {
-    return {
-      title: '',
-      description: '',
-      point: 0,
-      assign_to: ''
-    }
-  },
-  methods: {
-    addTask () {
-      this.$db.ref('task/log/').push({
-        title: this.title,
-        description: this.description,
-        point: this.point,
-        assign_to: this.assign_to,
-        status: 'log'
-      })
-      this.title = ''
-      this.description = ''
-      this.point = 0
-      this.assign_to = ''
-    }
+    done,
+    modal
   }
 }
 </script>
--- src/components/Log.vue
+++ src/components/Log.vue
@@ -12,39 +12,20 @@
         <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#myModal2" @click="getData(logs)">
          Details
         </button>
-        <!-- Modal -->
-       <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-         <div class="modal-dialog" role="document">
-           <div class="modal-content">
-             <div class="modal-header">
-               <h3 class="modal-title" id="exampleModalLabel">Details task "{{ dataTask.title }}" for {{ dataTask.assign_to }}</h3>
-               <button type="button" class="close" data-dismiss="modal">
-                 <span aria-hidden="true">&times;</span>
-               </button>
-             </div>
-             <div class="modal-body">
-              Task Description: {{ dataTask.description }},
-              Task Point: {{ dataTask.point }},
-              Status: {{ dataTask.status}}
-             </div>
-             <div class="modal-footer">
-               <button type="button" class="btn btn-danger" data-dismiss="modal" @click="remove(dataTask['.key'])">delete</button>
-               <button type="button" class="btn btn-primary" data-dismiss="modal" @click="moveToTodo(dataTask['.key'], dataTask.title, dataTask.description, dataTask.point, dataTask.assign_to)">todo</button>
-             </div>
-           </div>
-         </div>
-       </div>
-      </div>
+        <modalDetail :dataTask="dataTask "></modalDetail>
      </div>
     </div>
     </div>
   </div>
 </div>
-
 </template>
 
 <script>
+import modalDetail from '@/components/ModalDetail'
 export default {
+  components: {
+    modalDetail
+  },
   data () {
     return {
       dataTask: {}
@@ -59,26 +40,6 @@ export default {
     getData (log) {
       console.log('ini data log', log)
       this.dataTask = log
-    },
-    remove (id) {
-      if (window.confirm('delete this task')) {
-        this.$db.ref('task/log/' + id).remove()
-      }
-    },
-    moveToTodo (id, title, description, point, assign) {
-      if (window.confirm('move to todo')) {
-        this.$db.ref('task/todo').push({
-          title: title,
-          description: description,
-          point: point,
-          assign_to: assign,
-          status: 'todo'
-        })
-        this.$db.ref('task/log/' + id).remove()
-      }
-    },
-    created () {
-      this.show()
     }
   }
 }
